0ed578ba964003bf1a69f6248b51e26691e26249
soeceo <136312127+soeceo@users.noreply.github.com>
2023-07-19 22:47:13 +0800

    Add files via upload

---
+++ scripts/alpinessh.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# by https://github.com/soeceo/kxj
+# 2023.06.29
+
+if [ "$(cat /etc/os-release | grep -E '^ID=' | cut -d '=' -f 2)" != "alpine" ]
+then
+  echo "This script only supports Alpine Linux."
+  exit 1
+fi
+if [ "$(id -u)" -ne 0 ]
+then
+  echo "This script must be executed with root privileges."
+  exit 1
+fi
+apk add --no-cache openssh-server sshpass openssh-keygen
+if [ -f "/etc/motd" ]; then
+    echo '第一主机' >> /etc/motd
+    echo 'www.zhuji.one' >> /etc/motd
+fi
+cd /etc/ssh
+ssh-keygen -A
+sshport=22
+sed -i.bak '/^#PermitRootLogin\|PermitRootLogin/c PermitRootLogin yes' /etc/ssh/sshd_config
+sed -i.bak '/^#PasswordAuthentication\|PasswordAuthentication/c PasswordAuthentication yes' /etc/ssh/sshd_config
+sed -i.bak '/^#ListenAddress\|ListenAddress/c ListenAddress 0.0.0.0' /etc/ssh/sshd_config
+sed -i.bak '/^#AddressFamily\|AddressFamily/c AddressFamily any' /etc/ssh/sshd_config
+sed -i.bak "s/^#\?\(Port\).*/\1 $sshport/" /etc/ssh/sshd_config
+sed -i.bak -E 's/^#?(Port).*/\1 $sshport/' /etc/ssh/sshd_config
+/usr/sbin/sshd
+echo root:"$1" | chpasswd root
+rm -f "$0"
+
---
+++ scripts/build_monitor.sh
@@ -0,0 +1,15 @@
+﻿#!/bin/bash
+# from
+# https://github.com/soeceo/kxj
+# 2023.06.29
+
+# 检查 screen 是否已安装
+if ! command -v screen &> /dev/null; then
+    apt-get update
+    apt-get install -y screen
+fi
+
+curl -L https://github.com/soeceo/kxj/raw/main/scripts/monitor.sh -o monitor.sh && chmod +x monitor.sh
+
+# 启动一个新的 screen 窗口并在其中运行命令
+screen -dmS lxc_moniter bash monitor.sh
